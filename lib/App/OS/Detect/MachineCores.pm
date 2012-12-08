203455d69c5af35294fbb346e82d3527cc8b41e4
Daniel Bruder <daniel.bruder@gmail.com>
2012-12-08 18:58:33 +0100

    moo-ified version (gone away from mouse)

--- lib/App/OS/Detect/MachineCores.pm
+++ lib/App/OS/Detect/MachineCores.pm
@@ -3,63 +3,55 @@ package App::OS::Detect::MachineCores;
 #  PODNAME: App::OS::Detect::MachineCores
 # ABSTRACT: Detect how many cores your machine has (OS-independently)
 
-use Any::Moose;
-use 5.010;
 use true;
+use 5.010;
+use strict;
+use warnings;
 
-do {
-    with 'MooseX::Getopt';
-    with 'MooseX::Getopt::Dashes';
-} if Any::Moose::_is_moose_loaded();
+use Moo;
+use MooX::Options skip_options => [qw<os cores>];
 
-do {
-    with 'MouseX::Getopt';
-    with 'MouseX::Getopt::Dashes';
-} unless Any::Moose::_is_moose_loaded();
-
+has os => (
+    is       => 'ro',
+    required => 1,
+    default  => sub { $^O },
+);
 
 has cores => (
-    is         => 'ro',
-    isa        => 'Int',
-    traits     => [ 'NoGetopt' ],
-    lazy_build => 1,
-);
-has os => (
-    is         => 'ro',
-    isa        => 'Str',
-    traits     => ['NoGetopt'],
-    lazy_build => 1,
+    is      => 'rw',
+    isa     => sub { die "$_[0] is not a number!" unless $_[0] ~~ [0..100] },
+    lazy    => 1,
+    builder => '_build_cores',
 );
 
-has add_one => (
-    is            => 'ro',
-    isa           => 'Bool',
-    default       => 0,
-    traits        => [ 'Getopt', 'Bool' ],
-    cmd_aliases   => ['i'],
-    documentation => q{add one to the number of cores (useful in scripts)},
+option add_one => (
+    is      => 'rw',
+    isa     => sub { die "Invalid bool!" unless $_[0] ~~ [ 0 .. 1 ] },
+    default => sub { '0' },
+    short   => 'i',
+    doc     => q{add one to the number of cores (useful in scripts)},
 );
 
 sub _build_cores {
-    my $self = shift;
-    given ($self->os) {
-        when ('darwin') { $_ = `sysctl hw.ncpu | awk '{print \$2}'`; chomp; $_ }
-        when ('linux')  { $_ = `grep processor < /proc/cpuinfo | wc -l`; chomp; $_ }
-    }
+    do { 
+        given ($_[0]->os) {
+            when ('darwin') { $_ = `sysctl hw.ncpu | awk '{print \$2}'`;     chomp; $_ }
+            when ('linux')  { $_ = `grep processor < /proc/cpuinfo | wc -l`; chomp; $_ }
+        }
+    } or '0'
 }
 
-sub _build_os { $^O }
+around cores => sub {
+    my ($orig, $self) = (shift, shift);
 
-around 'cores' => sub {
-    my ($orig, $self, $set) = @_;
-    return $self->$orig() + 1 if $self->add_one and not defined $set;
-    return $self->$orig(); # otherwise
+    return $self->$orig() + 1 if $self->add_one;
+    return $self->$orig();
 };
 
+no Moo;
 
-no Any::Moose;
 
-__PACKAGE__->meta->make_immutable();
+=for Pod::Coverage os cores _build_cores add_one
 
 =begin wikidoc
 
@@ -76,8 +68,8 @@ This module will install one executable, {mcores}, in your bin.
 It is really simple and straightforward:
 
     usage: mcores [-?i] [long options...]
-        -? --usage --help  Prints this usage information.
-        -i --add-one       add one to the number of cores (useful in scripts)
+        -h --help       Prints this usage information.
+        -i --add_one    add one to the number of cores (useful in scripts)
 
 = SUPPORTED SYSTEMS
 * darwin (OSX)
@@ -91,10 +83,4 @@ Also it is quite handy to be able to increment the number by simply using a litt
 Example:
     export TEST_JOBS=`mcores -i`
 
-= WARNING
-Some questions with Dist::Zilla are still open, and although this module attempts to load [Mouse] instead of [Moose],
-unfortuantely, however, *both* modules are installed as prerequisites. This will change soon.
-
 =end wikidoc
-
-=cut
